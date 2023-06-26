925a2285651061bb03562a2a05e9cb4a44287e92
Karungg <miftahfadilah71@gmail.com>
2023-06-26 07:50:40 +0700

    Service Container

---
+++ app/Services/HelloServiceIndonesia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class HelloServiceIndonesia implements HelloService
+{
+    function hello(string $name): string
+    {
+        return "Hello $name";
+    }
+}
--- tests/Feature/DependencyInjectionTest.php
+++ tests/Feature/DependencyInjectionTest.php
@@ -4,6 +4,9 @@ namespace Tests\Feature;
 
 use App\Data\Bar;
 use App\Data\Foo;
+use App\Data\Person;
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -22,9 +25,94 @@ class DependencyInjectionTest extends TestCase
 
     public function testDependencyInjection()
     {
-        $foo = new Foo();
-        $bar = new Bar($foo);
+        // $foo = new Foo();
+        // $bar = new Bar($foo);
+
+        // self::assertEquals("Foo and Bar", $bar->bar());
+
+        $this->app->singleton(Foo::class, function ($app) {
+            return new Foo();
+        });
+
+        $foo = $this->app->make(Foo::class);
+        $bar = $this->app->make(Bar::class);
 
         self::assertEquals("Foo and Bar", $bar->bar());
+        self::assertSame($foo, $bar->foo);
+    }
+
+    public function testCreateDependency()
+    {
+        $foo = $this->app->make(Foo::class);
+        $foo2 = $this->app->make(Foo::class);
+
+        self::assertEquals("Foo", $foo->foo());
+        self::assertEquals("Foo", $foo2->foo());
+        self::assertNotSame($foo, $foo2);
+    }
+
+    public function testBind()
+    {
+        $this->app->bind(Person::class, function ($app) {
+            return new Person("Miftah", "Fadilah");
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals("Miftah", $person1->firstName);
+        self::assertEquals("Miftah", $person2->firstName);
+        self::assertNotSame($person1, $person2);
+    }
+
+    public function testSingleton()
+    {
+        $this->app->singleton(Person::class, function ($app) {
+            return new Person("Miftah", "Fadilah");
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals("Miftah", $person1->firstName);
+        self::assertEquals("Miftah", $person2->firstName);
+        self::assertSame($person1, $person2);
+    }
+
+    public function testInstance()
+    {
+        $person = new Person("Miftah", "Fadilah");
+        $this->app->instance(Person::class, $person);
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals("Miftah", $person1->firstName);
+        self::assertEquals("Miftah", $person2->firstName);
+        self::assertSame($person, $person1);
+        self::assertSame($person1, $person2);
+    }
+
+    public function testDependencyInjectionInClosure()
+    {
+        $this->app->singleton(Foo::class, function ($app) {
+            return new Foo();
+        });
+
+        $this->app->singleton(Bar::class, function ($app) {
+            return new Bar($app->make(Foo::class));
+        });
+
+        $bar1 = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+        self::assertSame($bar1, $bar2);
+    }
+
+    public function testHalloService()
+    {
+        $this->app->singleton(HelloService::class, HelloServiceIndonesia::class);
+
+        $helloService = $this->app->make(HelloService::class);
+        self::assertSame("Hello Miftah", $helloService->Hello("Miftah"));
     }
 }
