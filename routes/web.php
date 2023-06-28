f7f14b9fdce3a6ea7145761dff7a8324216dbd7c
Karungg <miftahfadilah71@gmail.com>
2023-06-28 09:38:22 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pzn', function () {
+    return "Miftah Fadilah Keren";
+});
+
+Route::redirect('/youtube', '/pzn');
+
+Route::fallback(function () {
+    return "404";
+});
